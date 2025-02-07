4185700a2f67d7844651faa77b902db7cbb9f531
princeprabhat <princeprabhat1996@gmail.com>
2025-02-07 15:53:02 +0530

    Input(): Controlled element Done

--- src/components/XDisplay.jsx
+++ src/components/XDisplay.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 const XDisplay = () => {
   const [fullName, setFullName] = useState("");
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
   const handleForm = (e) => {
     e.preventDefault();
 
@@ -14,10 +18,22 @@ const XDisplay = () => {
       <h1>Full Name Display</h1>
       <form onSubmit={handleForm}>
         <label>First Name: </label>
-        <input type="text" name="first-name" required />
+        <input
+          type="text"
+          name="first-name"
+          required
+          value={name.firstName}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+        />
         <br />
         <label>Last Name: </label>
-        <input type="text" name="last-name" required />
+        <input
+          type="text"
+          name="last-name"
+          required
+          value={name.lastName}
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
